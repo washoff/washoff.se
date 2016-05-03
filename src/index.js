fd7ce5d5795f4abf793ff28bd79d16a73915989e
Marcus Nielsen <marcus.nielsen82@gmail.com>
2016-05-03 17:40:43 +0200

    Adds initial code structure.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// TODO: Continue here. -MANI
+// import clientFactory from './client'
+
+function main() {
+  // clientFactory.create()
+  document.querySelector('[data-app-title]').text = 'Wash Off'
+}
+
+main()
